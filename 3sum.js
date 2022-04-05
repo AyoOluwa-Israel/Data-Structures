e9a8b8d164fe917bac3b102a27299816e47210e2
Adeleke Israel <izzyfresher@gmail.com>
2022-04-05 22:07:12 +0100

    3Sum

---
+++ 3sum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var threeSum = function(nums) {
+  nums = nums.sort((a,b)=> a-b);
+   let arr = [];
+  
+  for(let i = 0; i < nums.length-2; i++){
+      if(i > 0 && nums[i] === nums[i-1]){
+          continue;
+      }
+      
+      let left = i + 1;
+      let right = nums.length - 1;
+      
+      while(left < right){
+          if(nums[left] + nums[right] === -(nums[i])){
+              arr.push([nums[i], nums[left], nums[right]]);
+              while(left < right && nums[left] === nums[left+1]) left++;
+              while(left < right && nums[right] === nums[right-1]) right--;
+              left++;
+              right--;
+          } else if (nums[left] + nums[right] > -(nums[i])){
+              right--;
+          }else{
+              left++;
+          }
+      }
+  }
+  return arr;
+};
